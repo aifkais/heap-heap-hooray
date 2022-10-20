d801d391f22ef034b1f378ca4b06a0343db03d5d
aifkais <tonydang743@gmail.com>
2022-10-21 01:38:59 +0200

    s

---
+++ Assets/Pictures/DragDrop.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour , IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
+{
+
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
+
+    private void Awake(){
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData){
+        Debug.Log("OnPointerDown");
+        canvasGroup.alpha  = .6f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData){
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        Debug.Log("OnPointerDown");
+        canvasGroup.alpha = 1f;
+         canvasGroup.blocksRaycasts = true;
+    }
+
+   public void OnPointerDown(PointerEventData eventData){
+    Debug.Log("OnPointerDown");
+   }
+
+    public void OnDrop(PointerEventData eventData){
+        throw new System.NotImplementedException();
+    }
+
+}
