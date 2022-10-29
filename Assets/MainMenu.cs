89c1389f91ca6a9e02e4518245ddff98e0cc144b
Trizakz <leutemanneric@gmail.com>
2022-10-29 19:13:02 +0200

    Settings
    Added Settings Menu and Deleted Menu 1

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -9,9 +9,12 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     public void PlayGame()
     {
-        
-        
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+        SceneManager.LoadScene("Ingame");
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
     }
 
     public void QuitGame()
