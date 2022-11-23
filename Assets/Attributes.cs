185f844dbaf54ef2053f25b89ecda4b02d811f40
aifkais <tonydang743@gmail.com>
2022-11-23 20:07:52 +0100

    NewScene

--- Assets/Attributes.cs
+++ Assets/Attributes.cs
@@ -6,23 +6,13 @@ using UnityEngine.UI;
 public class Attributes : MonoBehaviour
 {
     public int wert=1;
-    public Sprite eins;
-    public Sprite zwei;
-    public Sprite drei;
-    public SpriteRenderer sr;
+    public int xwert = 1;
+    public int ywert = 1;
+    public int index  = 1;
+    public int nextNode = 1;
+    public bool isActive = 0;
 
 
-    public void changepic(){
-        if(this.wert == 1){
-            GetComponent<Image>().sprite = eins;
-        }
-        if(this.wert == 2){
-            GetComponent<Image>().sprite = zwei;
-        }
-        if(this.wert == 3){
-            GetComponent<Image>().sprite = drei;
-        }
-    }
 
     // Start is called before the first frame update
     void Start()
