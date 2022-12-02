aa64584189b0e857980c5d7d4e1bfcef2d3b53b0
aifkais <tonydang743@gmail.com>
2022-12-02 04:00:35 +0100

    DialogSystem

---
+++ Assets/TextBoxTrigger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBoxTrigger : MonoBehaviour
+{
+
+    [SerializeField] bool keyDown;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis ("Jump")!=0){
+		if(!keyDown){
+		animator.SetBool("IsOpen", true);
+		keyDown = true;	
+        
+		}
+    }
+}
+}
