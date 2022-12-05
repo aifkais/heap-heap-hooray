dc6ed3f4ecce8b843553a2641ce2cc216cdeadb3
Trizakz <leutemanneric@gmail.com>
2022-12-05 16:52:57 +0100

    Test

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -2,9 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+    public Toggle fullscreenToggle;
+
+
+    private void Start()
+    {
+        if (Screen.fullScreen == true)
+        {
+            fullscreenToggle.isOn = true;
+        }
+        else
+        {
+            fullscreenToggle.isOn = false;
+        }
+    }
+
+
     public void BackToMenu()
     {
         SceneManager.LoadScene("Menu");
